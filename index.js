ca15b1aca724cebb62d8b95cc6d40ee485a49d4f
marielkacabrera <mcabrera@cincinnatus.edu.do>
2025-08-27 11:03:56 -0400

    ahora si funciona el servidor

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', req, res => {
-    res.send('Coobright con Node!')
+// Ruta principal
+app.get('/', (req, res) => {
+    res.send('Coobright con Node!');
 });
 
+// Ruta con parámetro
 app.get('/saludo/:nombre', (req, res) => {
-  res.send(`Hola, ${req.params.nombre}! Bienvenido a la app desplegada 🎉`);
+    res.send(`¡Bienvenido, ${req.params.nombre}! Suscríbete a Coobright`);
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
 
 //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t

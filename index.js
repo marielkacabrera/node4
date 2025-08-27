3a7b483dfdb199f5d18110ef68ba58e30047b5c0
marielkacabrera <mcabrera@cincinnatus.edu.do>
2025-08-27 10:42:15 -0400

    ya no tengo idea de cuantos token genere porque git decia que lo puse mal

--- index.js
+++ index.js
@@ -14,3 +14,5 @@ app.get('/saludo/:nombre', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+//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t

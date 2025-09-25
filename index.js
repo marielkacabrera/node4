39acd8800f01abb0b54b6861cd6ac86d850cd608
marielkacabrera <mcabrera@cincinnatus.edu.do>
2025-09-25 09:06:05 -0400

    Add files via upload
    segundo proyecto

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 
-const PORT = process.env.PORT || 3000;
-
-// Ruta principal
 app.get('/', (req, res) => {
-    res.send('Coobright con Node!');
+    res.send('Esta Pagina es especial')
 });
 
-// Ruta con parámetro
-app.get('/saludo/:nombre', (req, res) => {
-    res.send(`¡Bienvenido, ${req.params.nombre}! Suscríbete a Coobright`);
+app.listen(PORT, () =>{
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
 });
-
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
-
-
-//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t
